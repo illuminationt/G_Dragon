85cbdcf3a58560cc568f2644febd6e6e6955e9f2
illumination <illuminationth@gmail.com>
2018-12-22 17:12:15 +0900

    コミット

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+    
+    public enum State
+    {
+
+    }
+
+
+    [SerializeField] protected int m_HP;
+    [SerializeField] protected int[] m_attack;
+    protected char m_hand;
+
+    protected virtual void DecideHand() { }
+    
+
+}
